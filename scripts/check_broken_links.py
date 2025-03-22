28697f2005e459d9168e46a37046be928acb8cfd
Henrique Sebastião <contato@henriquesebastiao.com>
2025-03-22 18:11:52 -0400

    ⛓️‍💥 ci: Adicionando workflow para verificação de links quebrados

---
+++ scripts/check_broken_links.py
@@ -0,0 +1,18 @@
+import sys
+
+import httpx
+
+with open('links.txt', 'r', encoding='utf-8') as file:
+    urls = [line.strip() for line in file if line.strip()]
+
+for link in urls:
+    try:
+        result = httpx.get(link)
+        if not 400 <= result.status_code <= 599:
+            print(f'{link} -> {result.status_code}')
+        else:
+            print(f'\nERRO -> Falha na URL {link} -> {result.status_code}')
+            sys.exit(1)
+    except Exception as error:
+        print(error)
+        sys.exit(1)
