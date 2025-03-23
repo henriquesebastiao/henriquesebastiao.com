4acdfd94d0cd3f27936b96a805863e47bf60dbaa
Henrique Sebastião <contato@henriquesebastiao.com>
2025-03-22 21:19:39 -0400

    🔧 ci: Retornando informações de debug em erros de requests no CI

--- scripts/check_broken_links.py
+++ scripts/check_broken_links.py
@@ -1,23 +1,35 @@
 import sys
 import ssl
 import certifi
+from pprint import pprint
 
 import httpx
+from rich.console import Console
+from rich.pretty import Pretty
+
+console = Console()
 
 with open('links.txt', 'r', encoding='utf-8') as file:
     urls = [line.strip() for line in file if line.strip()]
 
-context = ssl.create_default_context()
-context.load_verify_locations(certifi.where()) 
-client = httpx.Client(verify=context) 
-
 for link in urls:
     try:
-        result = client.get(link)
+        result = httpx.get(link)
         if not 400 <= result.status_code <= 599:
-            print(f'{link} -> {result.status_code}')
+            console.print(f'{link} -> [bold green]{result.status_code}[/bold green]')
         else:
-            print(f'\nERRO -> Falha na URL {link} -> {result.status_code}')
+            console.print(f'\n[bold red]ERRO[/bold red] -> Falha na URL {link} -> {result.status_code}')
+            print('\nRESPONSE:')
+            print('Headers:')
+            pprint(vars(result.headers))
+            print('Extensions:')
+            pprint(result.extensions)
+
+            print('\nREQUEST:')
+            pprint(result._request.method)
+            pprint(result._request.url)
+            pprint(result._request.headers)
+
             sys.exit(1)
     except Exception as error:
         print(error)
