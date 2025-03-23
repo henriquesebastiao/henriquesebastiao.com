c2813395ce05fb0d101e520ad15fb91fb48b2b3f
Henrique Sebastião <contato@henriquesebastiao.com>
2025-03-23 06:09:21 -0400

    ⚡ feat: Usando multithread para verificação de links quebrados

--- scripts/check_broken_links.py
+++ scripts/check_broken_links.py
@@ -1,8 +1,15 @@
 import sys
+from contextlib import contextmanager
+from datetime import datetime
 from pprint import pprint
+from queue import Queue
+from threading import Event, Thread
 
 import httpx
+from decouple import config
 from rich.console import Console
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 console = Console()
 client = httpx.Client()
@@ -10,21 +17,42 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0'
 }
 
-file = sys.argv[1]
+event = Event()
+fila = Queue(maxsize=101)
 
-with open(file, 'r', encoding='utf-8') as file:
-    urls = [line.strip() for line in file if line.strip()]
 
-for link in urls:
+def printd(*args):
+    if DEBUG:
+        print(*args)
+
+
+@contextmanager
+def timeit(*args):
+    start_time = datetime.now()
+    yield
+    time_elapsed = datetime.now() - start_time
+    print(f'Tempo de execução (hh:mm:ss.ms) {time_elapsed}')
+
+
+def get_links():
+    with open(sys.argv[1], 'r', encoding='utf-8') as file:
+        [fila.put(line.strip()) for line in file if line.strip()]
+    event.set()
+    fila.put('Kill')
+
+
+def check_broken_link(url):
     try:
-        result = client.get(link, headers=headers)
+        result = client.get(
+            url, headers=headers, timeout=10, follow_redirects=True
+        )
         if not 400 <= result.status_code <= 599:
             console.print(
-                f'{link} -> [bold green]{result.status_code}[/bold green]'
+                f'{url} -> [bold green]{result.status_code}[/bold green]'
             )
         else:
             console.print(
-                f'\n[bold red]ERRO[/bold red] -> Falha na URL {link} -> {result.status_code}'
+                f'\n[bold red]ERRO[/bold red] -> Falha na URL {url} -> {result.status_code}'
             )
             print('\nRESPONSE:')
             print('Headers:')
@@ -39,5 +67,60 @@ for link in urls:
 
             sys.exit(1)
     except Exception as error:
-        print(error)
+        print(f'\n{error}')
+        console.print(f'[bold red]ERRO[/bold red] -> Falha na URL {url}')
         sys.exit(1)
+
+
+def pipeline(*funcs):
+    def inner(argument):
+        state = argument
+        for func in funcs:
+            state = func(state)
+
+    return inner
+
+
+class Worker(Thread):
+    def __init__(self, target, queue, *, name='Worker'):
+        super().__init__()
+        self.name = name
+        self.queue = queue
+        self._target = target
+        self._stoped = False
+        printd(self.name, 'started')
+
+    def run(self):
+        event.wait()
+        while not self.queue.empty():
+            link = self.queue.get()
+            printd(self.name, link)
+            if link == 'Kill':
+                self.queue.put(link)
+                self._stoped = True
+                break
+            self._target(link)
+
+    def join(self):
+        while not self._stoped:
+            pass
+
+
+def get_pool(n_th: int):
+    """Retorna um número n de Threads."""
+    return [
+        Worker(target=target, queue=fila, name=f'Worker{n}')
+        for n in range(n_th)
+    ]
+
+
+target = pipeline(check_broken_link)
+
+with timeit():
+    get_links()
+    printd(fila.queue)
+    threads = get_pool(10)
+    printd('starts')
+    [th.start() for th in threads]
+    printd('joins')
+    [th.join() for th in threads]
